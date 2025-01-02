48cd2302649a5ada08fb3c0c63e7b8d0079a1499
axzilla <hello@axeladrian.com>
2025-01-02 13:24:58 +0700

    feat(auth): implement auth handler with error handling

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -11,15 +11,66 @@ import (
 	"github.com/axzilla/deeploy/internal/web/config"
 )
 
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	err := map[string]string{}
+	if email == "" {
+		err["email"] = "Email is required"
+	}
+	if password == "" {
+		err["password"] = "Password is required"
+	}
+
+	formData := map[string]string{
+		"email": email,
+	}
+
+	pages.Login(err, formData).Render(r.Context(), w)
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	passwordConfirm := r.FormValue("passwordConfirm")
+
+	err := map[string]string{}
+	if email == "" {
+		err["email"] = "Email is required"
+	}
+	if password == "" {
+		err["password"] = "Password is required"
+	}
+	if passwordConfirm == "" {
+		err["passwordConfirm"] = "Confirm your password"
+	}
+	if password != passwordConfirm {
+		err["password"] = "Passwords do not match"
+		err["passwordConfirm"] = "Passwords do not match"
+	}
+
+	formData := map[string]string{
+		"email": email,
+	}
+
+	pages.Register(err, formData).Render(r.Context(), w)
+}
+
 func main() {
 	config.LoadConfig()
 
 	mux := http.NewServeMux()
 
 	SetupAssetsRoutes(mux)
+
 	mux.Handle("GET /", templ.Handler(pages.Dashboard()))
-	mux.Handle("GET /login", templ.Handler(pages.Login()))
-	mux.Handle("GET /register", templ.Handler(pages.Register()))
+
+	mux.Handle("GET /login", templ.Handler(pages.Login(nil, nil)))
+	mux.HandleFunc("POST /login", loginHandler)
+
+	mux.Handle("GET /register", templ.Handler(pages.Register(nil, nil)))
+	mux.HandleFunc("POST /register", registerHandler)
 
 	fmt.Println("Server is running on http://localhost:8090")
 	http.ListenAndServe(":8090", mux)
