da81edcd977cce8f791e36df46b0c9819634e5de
axzilla <hello@axeladrian.com>
2025-01-05 15:22:12 +0700

    fix(assets): rename env thingy

--- internal/app/routes/assets.go
+++ internal/app/routes/assets.go
@@ -9,22 +9,16 @@ import (
 )
 
 func Assets(app deeploy.App) {
-	var isDevelopment = os.Getenv("GO_ENV") != "production"
-
+	var isDevelopment = os.Getenv("GO_ENV") != "prod"
 	assetHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if isDevelopment {
-			w.Header().Set("Cache-Control", "no-store")
-		}
-
 		var fs http.Handler
 		if isDevelopment {
+			w.Header().Set("Cache-Control", "no-store")
 			fs = http.FileServer(http.Dir("./internal/app/assets"))
 		} else {
 			fs = http.FileServer(http.FS(assets.Assets))
 		}
-
 		fs.ServeHTTP(w, r)
 	})
-
 	app.Router.Handle("GET /assets/", http.StripPrefix("/assets/", assetHandler))
 }
