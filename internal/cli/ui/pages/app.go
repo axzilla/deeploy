c1e0865d25b985e27904c07ad9f87f5efd9cbc8a
axzilla <hello@axeladrian.com>
2025-01-16 16:08:28 +0700

    feat(cli): remove unused files

--- internal/cli/ui/pages/app.go
+++ internal/cli/ui/pages/app.go
@@ -7,17 +7,13 @@ import (
 
 type AppModel struct {
 	currentView viewtypes.View
-	register    RegisterModel
 	initConnect InitConnectModel
-	login       LoginModel
 	width       int
 	height      int
 }
 
 func NewApp() *AppModel {
 	return &AppModel{
-		login:       NewLogin(0, 0),
-		register:    NewRegister(0, 0),
 		initConnect: NewInitConnect(0, 0),
 	}
 }
@@ -44,14 +40,6 @@ func (m *AppModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.initConnect = NewInitConnect(m.width, m.height)
 			m.currentView = viewtypes.InitConnect
 			return m, m.initConnect.Init()
-		case viewtypes.Login:
-			m.login = NewLogin(m.width, m.height)
-			m.currentView = viewtypes.Login
-			return m, m.login.Init()
-		case viewtypes.Register:
-			m.register = NewRegister(m.width, m.height)
-			m.currentView = viewtypes.Register
-			return m, m.register.Init()
 		case viewtypes.Dashboard:
 			m.currentView = viewtypes.Dashboard
 			return m, nil
@@ -64,22 +52,6 @@ func (m *AppModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	}
 
 	// Send updates to "children"
-	if m.currentView == viewtypes.Register {
-		model, cmd := m.register.Update(msg)
-		if register, ok := model.(RegisterModel); ok {
-			m.register = register
-		}
-		return m, cmd
-	}
-
-	if m.currentView == viewtypes.Login {
-		model, cmd := m.login.Update(msg)
-		if login, ok := model.(LoginModel); ok {
-			m.login = login
-		}
-		return m, cmd
-	}
-
 	if m.currentView == viewtypes.InitConnect {
 		model, cmd := m.initConnect.Update(msg)
 		if initConnect, ok := model.(InitConnectModel); ok {
@@ -94,10 +66,6 @@ func (m *AppModel) View() string {
 	switch m.currentView {
 	case viewtypes.InitConnect:
 		return m.initConnect.View()
-	case viewtypes.Register:
-		return m.register.View()
-	case viewtypes.Login:
-		return m.login.View()
 	case viewtypes.Dashboard:
 		return "Dashboard View"
 	}
