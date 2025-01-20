7399825e106a19366f5f6f88dff38942c7808d0b
axzilla <hello@axeladrian.com>
2025-01-20 21:26:37 +0700

    fix: bring back goenv and Plausible

--- internal/web/config/config.go
+++ internal/web/config/config.go
@@ -3,12 +3,14 @@ package config
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
 	CookieSecure bool
+	GoEnv        string
 }
 
 var AppConfig *Config
@@ -23,6 +25,7 @@ func LoadConfig() {
 
 	AppConfig = &Config{
 		// CookieSecure: os.Getenv("GO_ENV") != "dev",
+		GoEnv:        os.Getenv("GO_ENV"),
 		CookieSecure: false, // INFO: false for now because to many http/https cookie related issues (e.g login) while testing
 	}
 }
